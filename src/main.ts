261c923c1b7f4e5be8981620cebbabd5e88e1655
buingocthanh239 <buingocthanh2392002@gmail.com>
2024-11-07 10:42:15 +0700

    build: base log file with winston

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,39 @@ import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './exceptions/http-exception.filter';
 import { TransformInterceptor } from './interceptors/transform.interceptor';
 import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
+import { WinstonModule } from 'nest-winston';
+import { transports, format } from 'winston';
 import { configSwagger } from './config/api-docs.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        // let's log errors into its own file
+        new transports.File({
+          filename: `logs/error.log`,
+          level: 'error',
+          format: format.combine(format.timestamp(), format.json()),
+        }),
+        // logging all level
+        new transports.File({
+          filename: `logs/combined.log`,
+          format: format.combine(format.timestamp(), format.json()),
+        }),
+        // we also want to see logs in our console
+        new transports.Console({
+          format: format.combine(
+            format.cli(),
+            format.splat(),
+            format.timestamp(),
+            format.printf((info) => {
+              return `${info.timestamp} ${info.level}: ${info.message}`;
+            }),
+          ),
+        }),
+      ],
+    }),
+  });
   app.use(helmet());
 
   app.useGlobalPipes(new ValidationPipe());
