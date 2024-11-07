50da60264790468b2b2f2d6ec187361e8908c6fe
buingocthanh239 <buingocthanh2392002@gmail.com>
2024-11-07 18:03:33 +0700

    build: daily log file

--- src/main.ts
+++ src/main.ts
@@ -7,25 +7,31 @@ import { TransformInterceptor } from './interceptors/transform.interceptor';
 import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
 import { WinstonModule } from 'nest-winston';
 import { transports, format } from 'winston';
+import 'winston-daily-rotate-file';
 import { configSwagger } from './config/api-docs.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: WinstonModule.createLogger({
       transports: [
-        // let's log errors into its own file
-        new transports.File({
-          filename: `logs/error.log`,
+        // file on daily rotation (error only)
+        new transports.DailyRotateFile({
+          // %DATE will be replaced by the current date
+          filename: `logs/%DATE%-error.log`,
           level: 'error',
           format: format.combine(format.timestamp(), format.json()),
+          datePattern: 'YYYY-MM-DD',
+          zippedArchive: true, // don't want to zip our logs
+          maxFiles: '30d', // will keep log until they are older than 30 days
         }),
-        // logging all level
-        new transports.File({
-          filename: `logs/combined.log`,
+        // same for all levels
+        new transports.DailyRotateFile({
+          filename: `logs/%DATE%-combined.log`,
           format: format.combine(format.timestamp(), format.json()),
+          datePattern: 'YYYY-MM-DD',
+          zippedArchive: true,
+          maxFiles: '30d',
         }),
-
-        // we also want to see logs in our console
         new transports.Console({
           format: format.combine(
             format.cli(),
